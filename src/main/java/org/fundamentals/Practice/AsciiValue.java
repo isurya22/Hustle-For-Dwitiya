96ef04d20d0ca6eb98c767bbcbc263fb7f1dc058
surya <suryanarayan939@gmail.com>
2025-04-10 09:02:50 +0530

    Example of char,Byte,float and double type

---
+++ src/main/java/org/fundamentals/Practice/AsciiValue.java
@@ -0,0 +1,11 @@
+package org.fundamentals.Practice;
+
+public class AsciiValue {
+
+    public static void main(String[] args) {
+        char d = 63;
+        char e = 'S';
+        System.out.println(d);
+        System.out.println(e);
+    }
+}
---
+++ src/main/java/org/fundamentals/Practice/FractionalType.java
@@ -0,0 +1,17 @@
+package org.fundamentals.Practice;
+
+public class FractionalType
+{
+    public static void main(String[] args) {
+        float var1 = .3f;
+        float var2 = .2f;
+        float var3 = .3f - .2f;
+        System.out.println(var3);
+
+        double varA = 3.003;
+        double varB = 2.003;
+        double varC = varA - varB;
+        System.out.println(varC);
+    }
+
+}
