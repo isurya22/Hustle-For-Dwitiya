5896350e8c3e55a4f95ab42844c17eba03f916a0
surya <suryanarayan939@gmail.com>
2025-04-13 19:05:02 +0530

    Type of Casting

---
+++ src/main/java/org/java/fundamentals/Practice/WideningConversion.java
@@ -0,0 +1,13 @@
+package org.java.fundamentals.Practice;
+
+// This is also called as Automatic Widening.
+public class WideningConversion
+{
+    public static void main(String[] args) {
+        byte a = 23;
+        int b = 45;
+        int sum = a + b;
+
+        System.out.println(sum);
+    }
+}
