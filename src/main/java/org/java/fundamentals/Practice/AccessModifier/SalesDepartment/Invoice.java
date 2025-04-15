a53c41b7193e1f251a8331e7ebd7ca788ed8d567
surya <suryanarayan939@gmail.com>
2025-04-15 08:01:37 +0530

    Access Modifier

---
+++ src/main/java/org/java/fundamentals/Practice/AccessModifier/SalesDepartment/Invoice.java
@@ -0,0 +1,20 @@
+package org.java.fundamentals.Practice.AccessModifier.SalesDepartment;
+
+public class Invoice {
+
+    private int howManyInvoicePrint = 3;
+
+    protected void showIncome(){
+        System.out.println("Are we making profit or not ?");
+    }
+
+    public void getInvoice(){
+        System.out.println("Print Invoice");
+    }
+
+    public static void main(String[] args) {
+        Invoice invoice = new Invoice();
+        invoice.getInvoice();
+        System.out.println(invoice.howManyInvoicePrint);
+    }
+}
