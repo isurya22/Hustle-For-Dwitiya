5c01caaf114011a44a9fb9b201109a966c453147
surya <suryanarayan939@gmail.com>
2025-05-09 23:37:56 +0530

    Reflection of Classes

---
+++ src/main/java/org/java/Interface/Revision/Reflection/Jungle.java
@@ -0,0 +1,11 @@
+package org.java.Interface.Revision.Reflection;
+
+public class Jungle {
+    public static void main(String[] args) {
+        Class<Tiger> tigerClass = Tiger.class;
+        System.out.println(tigerClass);
+
+        System.out.println(tigerClass.getName());
+
+    }
+}
---
+++ src/main/java/org/java/Interface/Revision/Reflection/Tiger.java
@@ -0,0 +1,11 @@
+package org.java.Interface.Revision.Reflection;
+
+public class Tiger {
+    private String name;
+    public String gender;
+
+    public void run(){
+        System.out.println("FAST & FURIOUS");
+    }
+
+}
