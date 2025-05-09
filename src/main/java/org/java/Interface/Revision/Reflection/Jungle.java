c0d1ebed6a6b568ea6db63320f9ca632ffa283a2
surya <suryanarayan939@gmail.com>
2025-05-09 23:41:49 +0530

    Reflection of Classes

--- src/main/java/org/java/Interface/Revision/Reflection/Jungle.java
+++ src/main/java/org/java/Interface/Revision/Reflection/Jungle.java
@@ -1,11 +1,15 @@
 package org.java.Interface.Revision.Reflection;
 
+import java.util.Arrays;
+
 public class Jungle {
     public static void main(String[] args) {
         Class<Tiger> tigerClass = Tiger.class;
         System.out.println(tigerClass);
 
         System.out.println(tigerClass.getName());
+
+        System.out.println(Arrays.toString(tigerClass.getFields()));
 
     }
 }
