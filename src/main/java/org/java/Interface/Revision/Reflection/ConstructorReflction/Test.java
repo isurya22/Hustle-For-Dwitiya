ee96676a93faf8c777bd4f85efef153028c041b5
surya <suryanarayan939@gmail.com>
2025-05-10 23:30:11 +0530

    Reflection of Constructor

--- src/main/java/org/java/Interface/Revision/Reflection/ConstructorReflction/Test.java
+++ src/main/java/org/java/Interface/Revision/Reflection/ConstructorReflction/Test.java
@@ -1,17 +1,25 @@
 package org.java.Interface.Revision.Reflection.ConstructorReflction;
 
 import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Modifier;
 
 public class Test {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, InstantiationException, IllegalAccessException {
         Class<Eagle> eagleClass = Eagle.class;
 
         Constructor[] constructors = eagleClass.getDeclaredConstructors();
         for (Constructor constructor : constructors){
             System.out.println("Modifier: "+ Modifier.toString(constructor.getModifiers()));
             System.out.println("****");
+
+            /*
+              With help of reflection of constructor you can access private constructor and create a new object.
+             */
+            constructor.setAccessible(true);
+            Eagle eagle = (Eagle) constructor.newInstance(); //constructor called here.
+            eagle.fly();
         }
 
     }
