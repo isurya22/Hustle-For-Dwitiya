8f345dc14bd25cc90f7a4704bbe2543452cbbf2e
surya <suryanarayan939@gmail.com>
2025-08-05 21:47:50 +0530

    Unchecked Exception

---
+++ src/main/java/org/java/ExceptionHandling/Revision/ArithmaticException/ExamResult.java
@@ -0,0 +1,7 @@
+package org.java.ExceptionHandling.Revision.ArithmaticException;
+
+public class ExamResult {
+    public static void main(String[] args) {
+        int val = 20/0;
+    }
+}
--- src/main/java/org/java/ExceptionHandling/Revision/ClassCastException/Office.java
+++ src/main/java/org/java/ExceptionHandling/Revision/ClassCastException/Office.java
@@ -4,7 +4,8 @@ public class Office {
 
     public static void main(String[] args) {
         int EmployeeAge = 26;
-        // I am trying to print using casting to String.
+        /* I am trying to print using casting to String.
         System.out.println(String(EmployeeAge)); // Here we will find the Exception.
+         */
     }
 }
