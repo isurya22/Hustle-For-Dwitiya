130fbc23f0885b298b8145db57d12a2f8db87309
surya <suryanarayan939@gmail.com>
2025-08-05 19:09:04 +0530

    Unchecked Exception

---
+++ src/main/java/org/java/ExceptionHandling/Revision/ClassCastException/Office.java
@@ -0,0 +1,10 @@
+package org.java.ExceptionHandling.Revision.ClassCastException;
+
+public class Office {
+
+    public static void main(String[] args) {
+        int EmployeeAge = 26;
+        // I am trying to print using casting to String.
+        System.out.println(String(EmployeeAge)); // Here we will find the Exception.
+    }
+}
