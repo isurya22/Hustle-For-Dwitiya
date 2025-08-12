2e43ef70f6d64b9f80e128fbbd20ab860686fcf9
surya <suryanarayan939@gmail.com>
2025-08-12 21:33:18 +0530

    Learning Iterable Logic

---
+++ src/main/java/org/java/CollectionsFramework/Learn/IterableDemo.java
@@ -0,0 +1,29 @@
+package org.java.CollectionsFramework.Learn;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class IterableDemo {
+    // To Traverse the
+    public static void main(String[] args) {
+        List<Integer> values = new ArrayList<>(); //List is Interface & ArrayList is Concrete/Legacy Class.
+        values.add(2);
+        values.add(5);
+        values.add(9);
+        values.add(22);
+
+        System.out.println("List of Integer in ArrayList: "+ values);
+        Iterator<Integer> listOfNumbers = values.iterator();
+        while(listOfNumbers.hasNext()){                     // This method is from Iterator.
+             int val = listOfNumbers.next();                // This method is from Iterator.
+            System.out.println(val);
+            if(val == 9){
+                listOfNumbers.remove();
+            }
+        }
+        for(int value: values){
+            System.out.println("Remaining List of Integer in collection :" +value);
+        }
+    }
+}
