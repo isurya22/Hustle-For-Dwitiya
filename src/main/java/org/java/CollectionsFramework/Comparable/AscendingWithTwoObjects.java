6f9ceda9aef0d8ca0d033a59c8f663eeac27656a
surya <suryanarayan939@gmail.com>
2025-08-17 23:26:54 +0530

    Comparator Example

--- src/main/java/org/java/CollectionsFramework/Comparable/AscendingWithTwoObjects.java
+++ src/main/java/org/java/CollectionsFramework/Comparable/AscendingWithTwoObjects.java
@@ -13,7 +13,7 @@ public class AscendingWithTwoObjects {
         carList.add(new Car("sedan","disel"));
         carList.add(new Car("hatchback","cng"));
 
-        carList.sort(carList,(Car c1, Car c2)->c1.carType.compareTo(c2.carType));
+        Collections.sort(carList,(Car c1, Car c2)->c1.carType.compareTo(c2.carType));
 
         carList.forEach((Car c)-> System.out.println(c.carName +"....."+c.carType));
     }
