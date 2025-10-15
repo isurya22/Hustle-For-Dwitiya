fd2eb7b123a240f107dd4588bed619f5f74e75b8
surya <suryanarayan939@gmail.com>
2025-10-15 17:37:16 +0530

    Multithreading Example

---
+++ src/main/java/org/java/Multithreading/LearingS33/ProcessAndThreadDemo.java
@@ -0,0 +1,7 @@
+package org.java.Multithreading.LearingS33;
+
+public class ProcessAndThreadDemo {
+    public static void main(String[] args) {
+        System.out.println("Current Thread: "+Thread.currentThread().getName());
+    }
+}
