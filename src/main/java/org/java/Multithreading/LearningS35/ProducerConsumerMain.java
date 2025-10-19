fed7c2399e86809a0ff68ddb57293c9447398dd8
surya <suryanarayan939@gmail.com>
2025-10-19 15:00:11 +0530

    Scenario Producer-consumer Example

---
+++ src/main/java/org/java/Multithreading/LearningS35/ProducerConsumerMain.java
@@ -0,0 +1,32 @@
+package org.java.Multithreading.LearningS35;
+
+public class ProducerConsumerMain {
+    public static void main(String[] args) {
+        SharedValues sharedValues = new SharedValues(3);
+
+        //creating producer thread using lambda expression
+        Thread producerThread = new Thread(()->{
+            try{
+                for (int i = 0; i<=10; i++){
+                    sharedValues.produceItem(i);
+                }
+            }catch(Exception ex){
+                System.out.println(ex.getStackTrace());
+            }
+        });
+
+        //creating consumer thread using lambda expression
+        Thread consumerThread = new Thread(()->{
+            try{
+                for (int i = 0; i<=10; i++){
+                    sharedValues.consumeItem();
+                }
+            } catch (Exception ex){
+                System.out.println(ex.getStackTrace());
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
