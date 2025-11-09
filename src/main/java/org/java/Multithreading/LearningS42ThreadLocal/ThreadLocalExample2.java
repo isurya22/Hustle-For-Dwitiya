06b0ffe385eb036dc7eab98a1bc72996adcb3db4
surya <suryanarayan939@gmail.com>
2025-11-09 22:16:04 +0530

    ThreadLocal Explanation

--- src/main/java/org/java/Multithreading/LearningS42ThreadLocal/ThreadLocalExample2.java
+++ src/main/java/org/java/Multithreading/LearningS42ThreadLocal/ThreadLocalExample2.java
@@ -1,5 +1,26 @@
 package org.java.Multithreading.LearningS42ThreadLocal;
 
-public class ThreadLocalExample2 {
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
+public class ThreadLocalExample2 {
+      ThreadLocal<String> threadLocalObj = new ThreadLocal<>();
+
+      ExecutorService poolObj = Executors.newFixedThreadPool(5);
+
+      public ThreadLocalExample2(){
+          poolObj.submit(()-> {
+              threadLocalObj.set(Thread.currentThread().getName());
+          });
+
+          for(int i=1; i<15; i++){
+              poolObj.submit(()-> {
+                  System.out.println(threadLocalObj.get());
+              });
+          }
+      }
+
+    public static void main(String[] args) {
+        new ThreadLocalExample2();
+    }
 }
