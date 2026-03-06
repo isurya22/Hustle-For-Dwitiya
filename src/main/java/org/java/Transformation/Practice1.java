0bd5953eb8c25ca1edaa4999cc1e8764551d68c5
surya <suryanarayan939@gmail.com>
2026-03-06 23:37:32 +0530

    Practice on 6th March 2026

--- src/main/java/org/java/Transformation/Practice1.java
+++ src/main/java/org/java/Transformation/Practice1.java
@@ -8,7 +8,7 @@ public class Practice1 {
     public static void main(String[] args) {
         //Find the second-highest number
         List<Integer> numberList = Arrays.asList(6,22,10,50,3);
-        int secondHighestNumber = numberList.stream().distinct().sorted(Comparator.reverseOrder())
+        int secondHighestNumber = numberList.stream().sorted(Comparator.reverseOrder())
                 .skip(1).findFirst()
                 .orElseThrow(()->new RuntimeException("No second Highest Number"));
         System.out.println(secondHighestNumber);
