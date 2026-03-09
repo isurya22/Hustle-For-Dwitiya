104b9837c93a1ebc33cea695f11635cc4a93e69c
surya <suryanarayan939@gmail.com>
2026-03-10 00:17:48 +0530

    Practice 3 on 9th March

---
+++ src/main/java/org/java/Practice/Practice3On9thMarch.java
@@ -0,0 +1,16 @@
+package org.java.Practice;
+
+import java.util.Map;
+import java.util.stream.Collectors;
+
+public class Practice3On9thMarch {
+    //Count the frequency of character in a string
+    public static void main(String[] args) {
+        String word = "JaswanthKumar";
+
+        Map<Character, Long> result = word.toLowerCase().chars()
+                .mapToObj(e->(char)e)
+                .collect(Collectors.groupingBy(e->e, Collectors.counting()));
+        System.out.println(result);
+    }
+}
