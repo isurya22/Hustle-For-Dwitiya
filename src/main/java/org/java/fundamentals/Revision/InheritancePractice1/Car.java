5ef75f055c35576b903d5772316decb69c9fef41
surya <suryanarayan939@gmail.com>
2026-05-14 11:10:31 +0530

    Practice Inheritance

---
+++ src/main/java/org/java/fundamentals/Revision/InheritancePractice1/Car.java
@@ -0,0 +1,14 @@
+package org.java.fundamentals.Revision.InheritancePractice1;
+
+public class Car {
+    void drive(){
+        System.out.println("Car is driving");
+    }
+    void applybreak(){
+        System.out.println("Car can stop after apply break");
+    }
+
+    void mileage(){
+        System.out.println("Mileage can save your fuel");
+    }
+}
