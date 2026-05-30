ed3f3dceed9e6e7da9ffac963ebc93cd1fa309ee
surya <suryanarayan939@gmail.com>
2026-05-31 00:07:53 +0530

    Practice using Java 8

--- src/main/java/org/java/PracticeOnFunctionalInterface/PracticeFunctional.java
+++ src/main/java/org/java/PracticeOnFunctionalInterface/PracticeFunctional.java
@@ -1,5 +1,13 @@
 package org.java.PracticeOnFunctionalInterface;
 
-public class PracticeFunctional {
+import java.util.Map;
+import java.util.stream.Collectors;
 
+public class PracticeFunctional {
+    public static void main(String[] args) {
+        String str = "Suryanarayan Behera";
+        Map<Character, Long> count = str.toLowerCase().chars().mapToObj(c->(char)c)
+                .collect(Collectors.groupingBy(c->c,Collectors.counting()));
+        System.out.println(count);
+    }
 }
